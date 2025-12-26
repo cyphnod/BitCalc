e3fa1dedca5bd6786844b6d0f0f01ca18a3d8f20
Kvanox <218636129+cyphnod@users.noreply.github.com>
2025-12-26 16:05:46 +0100

    completion of main.py
    full code release for main.py [v2]

--- BitCalc_v2/main.py
+++ BitCalc_v2/main.py
@@ -25,3 +25,39 @@ class TradingCalculator(tk.Tk):
 
         self.tabs = ttk.Notebook(self)
         self.tabs.pack(expand = 1, fill="both")
+
+        # position size tab
+
+        self.tab_position = ttk.Frame(self.tabs)
+        self.tabs.add(self.tab_position, text="Position Size")
+
+        self.create_position_tab()
+
+    def create_position_tab(self):
+        tk.Label(self.tab_position, text="Account Balance:").grid(row=0, column=0, padx=10, pady=10)
+        tk.Label(self.tab_position, text="Risk %:").grid(row=1, column=0, padx=10, padxy=10)
+        tk.Label(self.tab_position, text="Stop Loss:").grid(row=2, column=0, padx=10, pady=10)
+
+        self.balance_entry = tk.Entry(self.tab_position)
+        self.risk_entry = tk.Entry(self.tab_position)
+        self.stop_entry = tk.Entry(self.tab_position)
+
+        self.balance_entry.grid(row=0, column=1)
+        self.risk_entry.grid(row=1, column=1)
+        self.stop_entry.grid(row=2, column=1)
+
+        tk.Button(self.tab_position, text="Calculate", command=self.calculate_position).grid(row=3, column=0, columnspan=2, pady=10)
+
+        self.result_label = tk.Label(self.tab_position, text="Position size: ")
+        self.result_label.grid(row=4, column=0, columnspan=2, pady=10)
+
+    def calculate_position(self):
+        balance = self.balance_entry.get()
+        risk = self.risky_entry.get()
+        stop = self.stop_entry.get()
+        result = position_size(balance, risk, stop)
+        self.result_label.config(text=f"Position Size: {result}")
+
+if __name__ == "__main__":
+    app = TradingCalculator()
+    app.mainloop()
